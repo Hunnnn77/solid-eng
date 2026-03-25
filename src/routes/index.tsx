e4621b594823c914ddd66d8996a99aebc4b1a6b4
hun <k01084758975dev@gmail.com>
2026-03-25 16:59:59 +0900

    minor3

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -15,7 +15,113 @@ import { Input } from "~/components/Input";
 import type { TAction } from "~/types";
 import { marked } from "marked";
 import { Button } from "~/components/Button";
-import { analyzeAction, paragraphAction, transcriptionAction, wordAction } from "~/actions";
+import { action } from "@solidjs/router";
+import { streamText } from "ai";
+import dedent from "dedent";
+import { deepseek } from "~/client/llm";
+import { fetchTranscript } from "youtube-transcript-plus";
+
+const wordAction = action(async (q: string) => {
+  "use server";
+
+  const system = dedent`
+  Here is output format
+
+  **Meaning**
+  **Part of Speech**
+  **Examples**
+    1.
+    2.
+    3.
+    4.
+    5.
+  `;
+  const prompt = `Please define the meaning and part of speech for this word, and provide five example sentences.: ${q}`;
+  const { textStream } = streamText({
+    system,
+    model: deepseek(process.env.DEEPSEEK_API)("deepseek-chat"),
+    prompt,
+  });
+
+  return {
+    stream: textStream,
+  };
+}, "word");
+
+const paragraphAction = action(async (q: string) => {
+  "use server";
+
+  const system = dedent`
+  Here is output format
+
+  **Overral Analysis**
+  **Grammar**
+  **Spelling**
+  **Improvements**
+    1.
+    2.
+    3.
+    4.
+    5.
+  **Improved Sentence**
+    1.
+    2.
+    3.
+    4.
+    5.
+  `;
+  const prompt = `Please analyze my sentence, provide an evaluation and spell check, and suggest an improved version.: ${q}`;
+  const { textStream } = streamText({
+    system,
+    model: deepseek(process.env.DEEPSEEK_API)("deepseek-chat"),
+    prompt,
+  });
+
+  return {
+    stream: textStream,
+  };
+}, "paragraph");
+
+const transcriptionAction = action(async (dialogValue: string) => {
+  "use server";
+
+  const transcription = await fetchTranscript(dialogValue, {
+    userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:148.0) Gecko/20100101 Firefox/148.0",
+  })
+    .then((r) => r.map((trans) => trans.text).join(" "))
+    .catch((e) => {
+      if (e instanceof Error) {
+        return e.message;
+      }
+      return "panic!";
+    });
+
+  return {
+    transcription,
+  };
+});
+
+const analyzeAction = action(async (transcription: string) => {
+  "use server";
+
+  const system = dedent`
+  Here is output format
+
+  **Brief Summary**
+  **Repetitive Words**
+  **Commonly used Sentences**
+  `;
+  const prompt = `Please analyze sentences: ${transcription}`;
+  const { textStream } = streamText({
+    system,
+    model: deepseek(process.env.DEEPSEEK_API)("deepseek-chat"),
+    prompt,
+  });
+
+  return {
+    stream: textStream,
+  };
+});
 
 export default function Home() {
   const transcription = useAction(transcriptionAction);
